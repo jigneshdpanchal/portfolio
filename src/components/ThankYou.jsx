6cb466348d503945d94414697e36f3a71768efc4
jigneshdpanchal <jigneshp90@gmail.com>
2023-09-28 03:29:00 +0530

    Thank You Message Button Link Updated

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -6,7 +6,7 @@ const ThankYou = () => {
         <h2 className='text-[22px] text-white font-bold'>Thank You for Contacting Us!</h2><br/>
         <p className='text-white '>We've received your message and will get back to you as soon as possible. Your input is important to us, and we appreciate your interest. In the meantime, feel free to explore more of our website or check out our latest updates.</p>
         <h4 className='m-10 text-white font-semibold text-lg'>Have a great day!</h4>
-            <a href='/'><button className='py-2 px-10 rounded-md bg-black text-white hover:bg-white hover:text-black'>Back to Home</button></a>
+            <a href='https://jigneshdpanchal.github.io/portfolio/'><button className='py-2 px-10 rounded-md bg-black text-white hover:bg-white hover:text-black'>Back to Home</button></a>
     </div>
   )
 }
